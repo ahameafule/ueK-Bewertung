9ac780fe5975fe952f8435fa4f9f363943b2f5ec
Unknown <j.ahameafule@gmx.ch>
2019-03-27 14:33:21 +0100

    Spring initialize

---
+++ src/main/java/ch/noseryoung/uekbewertung/UekBewertungApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.uekbewertung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UekBewertungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UekBewertungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/noseryoung/uekbewertung/UekBewertungApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.noseryoung.uekbewertung;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UekBewertungApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
